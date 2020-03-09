ce3b8b79ba7a4b64d24256fe5bd548ad7713a862
Abedin Hassan <abedin.hassan.60@gmail.com>
2020-03-09 15:13:40 +0600

    Fixed Search Functionality

--- GRE_DICTIONARY/Dictionary.xaml.cs
+++ GRE_DICTIONARY/Dictionary.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -33,21 +34,61 @@ namespace GRE_DICTIONARY
 
         private void search_btn_click(object sender, RoutedEventArgs e)
         {
-            //DataGrid dg = (DataGrid)sender;
-            //DataRowView row_selected = dg.SelectedItem as DataRowView;
+            string searchText = textBox.Text;
+	        if(searchText!="")
+	        {
+                string con = @"Data Source=.;Initial Catalog=GRE_DICTIONARY_db;Integrated Security=True";
+		        SqlConnection sqlcon = new SqlConnection(con);
+		        sqlcon.Open();
 
-            //if (row_selected != null)
-            //{
-            //    //serial_txt.Text = row_selected["Serial No"].ToString();
-            //    //word_txt.Text = row_selected["Word"].ToString();
-            //    //eng_meaning.Text = row_selected["English Meaning"].ToString();
-            //    //prtsof_speech.Text = row_selected["Parts of Speech"].ToString();
-            //}
+		        string sqlquery = "Select * from Table_3 where word='"+searchText+"'";
+		        SqlCommand sqlcmd = new SqlCommand(sqlquery, sqlcon);
 
+		        SqlDataAdapter data_adapter = new SqlDataAdapter(sqlcmd);
+		        DataTable dt = new DataTable("Table_3");
+		        data_adapter.Fill(dt);
+		        dataGrid.ItemsSource = dt.DefaultView;
+		        data_adapter.Update(dt);
+		        sqlcon.Close();
+	        }
+	        else 
+	        {
+                string con = @"Data Source=.;Initial Catalog=GRE_DICTIONARY_db;Integrated Security=True";
+		        SqlConnection sqlcon = new SqlConnection(con);
+		        sqlcon.Open();
+
+		        string sqlquery = "Select * from Table_3";
+		        SqlCommand sqlcmd = new SqlCommand(sqlquery, sqlcon);
+
+		        SqlDataAdapter data_adapter = new SqlDataAdapter(sqlcmd);
+		        DataTable dt = new DataTable("Table_3");
+		        data_adapter.Fill(dt);
+		        dataGrid.ItemsSource = dt.DefaultView;
+		        data_adapter.Update(dt);
+		        sqlcon.Close();
+	        }
+
+	    }
+
+        private void clearDataGrid()
+        {
+            string con = @"Data Source=.;Initial Catalog=GRE_DICTIONARY_db;Integrated Security=True";
+            SqlConnection sqlcon = new SqlConnection(con);
+            sqlcon.Open();
+
+            string sqlquery = "Select * from Table_3 where word=''";
+            SqlCommand sqlcmd = new SqlCommand(sqlquery, sqlcon);
+
+            SqlDataAdapter data_adapter = new SqlDataAdapter(sqlcmd);
+            DataTable dt = new DataTable("Table_3");
+            data_adapter.Fill(dt);
+            dataGrid.ItemsSource = dt.DefaultView;
+            data_adapter.Update(dt);
+            sqlcon.Close();
         }
 
-        private void dictionary_loaded(object sender, RoutedEventArgs e)
-        {
+	private void dictionary_loaded(object sender, RoutedEventArgs e)
+	{
             string con = @"Data Source=.;Initial Catalog=GRE_DICTIONARY_db;Integrated Security=True";
             SqlConnection sqlcon = new SqlConnection(con);
             sqlcon.Open();
